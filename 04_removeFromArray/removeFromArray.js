497b7b05420ca20048ce99e0a77e66ec64557ea0
Sarah Good <sarah.elizabeth.good@gmail.com>
2025-01-04 14:00:08 -0800

    pass removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    newArray = [];
+    for (let i=0; i<myArray.length; i++) {
+        if (!args.includes(myArray[i])) {
+            newArray.push(myArray[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
