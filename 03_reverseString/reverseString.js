cee6ed9c024a3474f8a37166a045cb222c776c9f
Sarah Good <sarah.elizabeth.good@gmail.com>
2025-01-04 13:49:03 -0800

    pass reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    stringArray = words.split('');
+    outputString = '';
+    for (let i = 1; i <= stringArray.length; i++) {
+        outputString += stringArray.at(-i);
+    }
+    return outputString;
 };
 
 // Do not edit below this line
