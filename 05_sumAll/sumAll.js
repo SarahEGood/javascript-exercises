b3cecd8d156bfbc99393cbf118ef92e570e1432a
Sarah Good <sarah.elizabeth.good@gmail.com>
2025-01-04 14:16:55 -0800

    pass sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(ind1, ind2) {
+    if ((typeof ind1 != 'number') || (typeof ind2 != 'number')) {
+        return 'ERROR';
+    } else if ((ind1 < 0) || (ind2 < 0) ||
+                (!Number.isInteger(ind1)) || (!Number.isInteger(ind2))) {
+        return 'ERROR';
+    } else {
+        totalSum = 0;
+        numSorted = [ind1, ind2].sort();
+        for (let i = numSorted[0]; i <= numSorted[1]; i++) {
+            totalSum += i;
+        }
+        return totalSum;
+    }
 };
 
 // Do not edit below this line
